b7bdec3d3954b9b044699c548997063efff426b7
Daniel Zaleta <zaletadaniel@gmail.com>
2022-07-10 20:16:56 -0500

    Context and Reducer

---
+++ src/context/places/PlacesProvider.tsx
@@ -0,0 +1,32 @@
+import { useReducer } from "react";
+import { PlacesContext } from "./PlacesContext";
+import { placesReducer } from "./placesReducer";
+
+export interface PlacesState {
+    isLoading: boolean;
+    userLocation?: [number, number];
+}
+
+const INITIAL_STATE: PlacesState = {
+    isLoading: true,
+    userLocation: undefined,
+}
+
+interface Props {
+    children: JSX.Element | JSX.Element[]
+}
+
+const PlacesProvider = ({ children }: Props) => {
+
+    const [state, dispatch] = useReducer(placesReducer, INITIAL_STATE);
+
+    return (
+        <PlacesContext.Provider value={{
+            ...state
+        }}>
+            {children}
+        </PlacesContext.Provider>
+    )
+}
+
+export { PlacesProvider }
